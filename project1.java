f48b82c91a07595e0c30963aa8ac06e8686cef63
Prerak <coolsunny1811@gmail.com>
2017-02-13 14:21:12 +0530

    2

--- project1.java
+++ project1.java
@@ -1,11 +1,11 @@
 class main{
 	public static void main(String args[]){
-<<<<<<< HEAD
+
 		for(int i=0; i<10;i++){
 			System.out.println("This is a project1 like hello2 // hello world again..");
 		}	
-=======
+
 		System.out.println("This is a project1 like hello2 // hello world again..");
->>>>>>> 42a011e151a5cd3f6cdf74b42d3223cd2d0fce49
+
 	}
 }
